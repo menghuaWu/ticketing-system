b12d72ce52079b770f05e1d5c6bfc432c50f1689
TAXI\scott.wu <scott.wu@55688.com.tw>
2025-03-04 17:02:26 +0800

    修改為SSR啟動

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,14 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: true, // 確保啟用 SSR
   css: [
     '@/assets/css/tailwind.css'
   ],
+
+  buildDir: '.nuxt',
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  nitro: {
+    preset: 'node', // 使用 Node.js 預設模式
+    output: {
+      dir: '.output'
+    },
+    serveStatic: true, // 確保靜態文件能正確載入
+    logLevel: 3, // 設置日誌級別，方便偵錯
+  },
+  vite: {
+    build: {
+      sourcemap: false, // 防止 source map 載入錯誤
+    }
+  },
+  compatibilityDate: '2025-03-04'
 })
