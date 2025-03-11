50599a0f0a6566c58310c531341efb822f07bc13
TAXI\scott.wu <scott.wu@55688.com.tw>
2025-03-11 15:20:21 +0800

    feat:新增component組件顯示Ticket列表

---
+++ frontend/components/TicketList.vue
@@ -0,0 +1,15 @@
+<script>
+const {data: tickets, pending, error} = useTickets()
+</script>
+
+<template>
+    <div v-if="pending">Loading tickets...</div>
+    <div v-else-if="error">Failed to load tickets</div>
+    <div v-else>
+        <ul>
+            <li v-for="ticket in tickets" :key="ticket.id">
+                {{ ticket.title }} - {{ ticket.price }}$
+            </li>
+        </ul>
+    </div>
+</template>
